02b9569ff3f42edfaa82e820882f4bd22a4fe328
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-09-12 04:25:15 +0000

    comparisons

---
+++ 01_basics/04_comparisonss.js
@@ -0,0 +1,12 @@
+// console.log(2 == 2); //true
+// console.log(2 != 2); //false
+
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+
+console.log("2" === 2);//false
+console.log("2" == 2);//true
+
