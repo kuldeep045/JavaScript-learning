ec60ab1b1613ffee14a601ebdf4e36420da5bd9d
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-09-12 04:17:35 +0000

    operation

---
+++ 01_basics/03_conversionAndOperation.js
@@ -0,0 +1,25 @@
+let a, b;
+a = 3;
+b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(a % b);
+
+
+console.log(4 + "2"); //42
+console.log("4" + 2); //42
+console.log("4" - 2); //2
+
+
+console.log("1" + 3 + 5); //135
+console.log(1 + 3 + "5"); //45
+
+
+// ------------prefix and post fix operator--------
+
+let counter = 100;
+console.log(counter++); //100 increase val after code execution
+console.log(++counter); //102 increase val and then execute other code
