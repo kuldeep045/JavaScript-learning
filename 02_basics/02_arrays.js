ed9d3d8d0f83ce40e652b8aa5f4751e54b938050
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-09-13 23:29:07 +0000

    fbcf

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,6 @@
+let marvelHeroes = ["thor", "Spiderman", "Ironman"];
+let dcHeroes = ["Superman", "Flash", "Batman"];
+
+
+console.log(marvelHeroes.push(dcHeroes));
+console.log(marvelHeroes.concat(dcHeroes));
