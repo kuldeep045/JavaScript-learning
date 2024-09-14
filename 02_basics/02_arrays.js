51f9e53a0ff917996be096b6c9a745d138554121
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-09-14 06:32:46 +0000

    array completed

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -2,5 +2,18 @@ let marvelHeroes = ["thor", "Spiderman", "Ironman"];
 let dcHeroes = ["Superman", "Flash", "Batman"];
 
 
-console.log(marvelHeroes.push(dcHeroes));
-console.log(marvelHeroes.concat(dcHeroes));
+//console.log(marvelHeroes.push(dcHeroes)); //add second array as an element in first array
+//console.log(marvelHeroes.concat(dcHeroes)); //add all elements of arrray second as elements in first array
+
+
+let newHeroes = [...dcHeroes,...marvelHeroes]//spreadOperator
+//console.log(newHeroes);
+
+
+let anotherArr = [1, 2, [3, 4, 5], 6, 7, [8,11, [9, 10]]];
+let realAnotherArr = anotherArr.flat(Infinity);
+//console.log(realAnotherArr);
+
+console.log(Array.isArray("Cooldeep"))
+console.log(Array.from("Cooldeep"))
+console.log(Array.from({name: "Cooldeep"})) //gives empty array
