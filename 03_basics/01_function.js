e92c8eb95be703887691bd6d9c4b7922f928bcd5
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-09-15 00:07:31 +0000

    functions done

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -16,7 +16,7 @@ function sum(num1, num2){
     console.log("Hii Kuldeep ") //unreachable code
 }
 let res = sum(2, 4);
-console.log("Sum is : ", res) //undefined if return statement is not given
+//console.log("Sum is : ", res) //undefined if return statement is not given
 
 //values passed during calling is called as argument
 
@@ -24,4 +24,19 @@ console.log("Sum is : ", res) //undefined if return statement is not given
 function sayHii (userName = 'Sire'){
     return `${userName} kaise ho aap`
 }
-console.log(sayHii()); //if value not passed defalut value sore will be passed here....
+//console.log(sayHii()); //if value not passed defalut value sore will be passed here....
+
+
+function calculateCArtPricce(...num){
+    return num
+}
+//console.log(calculateCArtPricce(1, 2, 3, 4)) //makes array of all passed arguments
+
+function handleObj (obj){
+    console.log(`${obj.name} your nickname is ${obj.nickName}`)
+}
+let objOne = {
+    name: 'kuldeep',
+    nickName: 'Cooloo baby'
+}
+handleObj(objOne);
