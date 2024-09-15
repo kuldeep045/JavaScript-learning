888267c5ed1fe17af82bbece7d5c5b0ce36981dc
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-09-15 00:58:14 +0000

    more on scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,5 +1,31 @@
 let a = 1; //has only block scope
 const b = 2; //has only block scope
 var c = 3; //it has global scope so create problems sometimes
-console.log(a, b, c);
+//console.log(a, b, c);
 
+
+
+function one (){
+    let outer = 1;
+
+    function two(){
+        let inner = 2;
+        console.log(outer) //can access parents' variable
+    }
+   // console.log(inner) //cannot access here
+    two();
+}
+//one();
+
+addOne(8)//can access here
+function addOne (num){
+    console.log(num + 1)
+
+}
+
+
+//addTwo(8) //cant access here before initialization
+const  addTwo = function (num){
+    console.log(num + 2)
+
+}
