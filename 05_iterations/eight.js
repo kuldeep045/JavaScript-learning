e8698aa19c5025f385ae8881724ed37bbda8b40a
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-09-21 02:02:28 +0000

    iterations done

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -2,8 +2,8 @@ let myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 let Total = myNums.reduce( (acc, currval)=> {
- //   console.log(`acc is ${acc} and currval is ${currval}`)
+    console.log(`acc is ${acc} and currval is ${currval}`)
 
     return acc + currval;
-})
-console.log(Total);
+}, 0)
+//console.log(Total);
